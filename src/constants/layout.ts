46b2d7b33208d2eb8c3d346b7a13597a6affd1b4
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2026-06-01 10:56:12 +0545

    upadte in the frontend for the layout a nimation

---
+++ src/constants/layout.ts
@@ -0,0 +1,13 @@
+export const LAYOUT = {
+  screenPaddingX: 20,
+  screenPaddingTop: 16,
+  screenPaddingBottom: 24,
+  sectionGap: 16,
+  fieldGap: 10,
+  cardRadius: 16,
+  headerHeight: 56,
+  inputHeight: 54,
+  buttonHeight: 56,
+  bottomSheetRadius: 24,
+  heroHeight: 140,
+} as const;
